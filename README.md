687dd10d64b76507ec732d7fecc54f3572d792ac
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-15 11:29:18 +0100

    updated bankist app

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# Project Name
+
+Bankist App
+
+> Description the project.
+The Website was built fully functional with javascript, it uses dom manipulation to update the html dynamically with effects that makes the whole website interactive and user friendly.
+
+
+
+
+## Built With
+
+- HTML
+- Css
+- Javascript
+
+## Portfolio Live Version 
+
+[Live Demo Link](https://efiamotu-1.github.io/Bankist-App)
+
+## Authors
+
+👤 **Habeeb Musa Efiamotu**
+
+- GitHub: [@githubhandle](https://github.com/Efiamotu-1)
+- Twitter: [@twitterhandle](https://twitter.com/EFYAMOTU)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/musa-habeeb/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header')
 const btnScrollTo = document.querySelector('.btn--scroll-to')
 const section1 = document.querySelector('#section--1')
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('nav')
 
 
 const openModal = function () {
@@ -70,6 +75,102 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
   }
 })
 
+
+
+tabsContainer.addEventListener('click', function (e) {
+  
+  const clicked = e.target.closest('.operations__tab')
+  
+  console.log(clicked)
+
+  // Guard clause 
+  if(!clicked) return
+  console.log(clicked.dataset.tab)
+// Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'))
+  // setting Active tab
+  clicked.classList.add('operations__tab--active');
+  
+  // Activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+
+});
+
+//  Menu fade animation
+const handleHover = function(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this
+    })
+
+    logo.style.opacity = this
+  }
+}
+
+// passing arguments into handler using bind
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// sticky navigation(Scroll)
+
+const initialCoords = section1.getBoundingClientRect()
+// console.log(initialCoords)
+window.addEventListener('scroll', function(e) {
+
+  // Intersection observer Api for add sticky
+  const obsCallback = function(entries, observer) {
+    entries.forEach(entry => {
+      console.log(entry)
+    })
+  };
+
+  const obsOption = {
+    root : null,
+    threshold : 0.1
+  };
+
+  const observer = new IntersectionObserver(obsCallback, obsOption)
+  observer.observe(section1)
+
+  // Old way of using scroll
+  // teachers way of writing if statements
+  // if (window.scrollY > initialCoords) nav.classList.add('sticky')
+  // else nav,classList.remove('sticky')
+  // my usual way of writing if statements
+  // if (window.scrollY > initialCoords.top) {
+  //   nav.classList.add('sticky')
+  // }
+  // else {
+  //   nav.classList.remove('sticky')
+  // }
+})
+
+
+// Fade using a callback to call another function with arguments
+// arguments shouldnt be passed into a call back function
+// const handleHover = function(e, opacity) {
+//   if (e.target.classList.contains('nav__link')) {
+//     const link = e.target;
+//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+//     const logo = link.closest('.nav').querySelector('img');
+
+//     siblings.forEach(el => {
+//       if (el !== link) el.style.opacity = opacity
+//     })
+
+//     logo.style.opacity = opacity
+//   }
+// }
+
+// nav.addEventListener('mouseover', (e) => handleHover(e, 0.5));
+
+// nav.addEventListener('mouseout', (e) => handleHover(e, 1));
 
 
 
@@ -168,31 +269,31 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
 const h1 = document.querySelector('h1');
 
 // Going downwards selecting child elements
-console.log(h1.querySelectorAll('.highlight'))
-console.log(h1.childNodes);
-console.log(h1.children)
-h1.firstElementChild.style.color = 'white'
-h1.lastElementChild.style.color = 'orangered'
+// console.log(h1.querySelectorAll('.highlight'))
+// console.log(h1.childNodes);
+// console.log(h1.children)
+// h1.firstElementChild.style.color = 'white'
+// h1.lastElementChild.style.color = 'orangered'
 
-// Going upwards selecting parents
+// // Going upwards selecting parents
 
-console.log(h1.parentNode)
-console.log(h1.parentElement)
+// console.log(h1.parentNode)
+// console.log(h1.parentElement)
 
-h1.closest('.header').style.background = 'var(--gradient-secondary)'
+// h1.closest('.header').style.background = 'var(--gradient-secondary)'
 
-// Going sideways selecting siblings
+// // Going sideways selecting siblings
 
-console.log(h1.previousElementSibling)
-console.log(h1.nextElementSibling)
+// console.log(h1.previousElementSibling)
+// console.log(h1.nextElementSibling)
 
-console.log(h1.previousSibling)
-console.log(h1.nextSibling)
+// console.log(h1.previousSibling)
+// console.log(h1.nextSibling)
 
-console.log(h1.parentElement.children);
+// console.log(h1.parentElement.children);
 
-[...h1.parentElement.children].forEach(function(el) {
-  if(el !== h1) {
-    el.style.transform = 'scale(0.5)'
-  }
-})
+// [...h1.parentElement.children].forEach(function(el) {
+//   if(el !== h1) {
+//     el.style.transform = 'scale(0.5)'
+//   }
+// })
